13dfd272f24bec787af57c1451f46e63608d8cc2
akshaypalshikar <aksmakesit@gmail.com>
2018-07-03 19:49:48 -0700

    added uesr input and user output

---
+++ src/User/UserInput.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+import { ReactDOM } from 'react-dom';
+
+const UserInput = (props) => {
+    return(
+        <div>
+            <input type="text"></input>
+        </div>
+    );
+}
---
+++ src/User/UserOutput.js
@@ -0,0 +1,25 @@
+import { React } from 'react';
+import { ReactDOM } from 'react-dom';
+
+const UserInput = (props) => {
+    return(
+        <div>
+            <p>
+                To https://github.com/akshaypalshikar/react-assignment-1.git
+                * [new branch] step-1 -> step-1
+                Branch step-1 set up to track remote branch step-1 from origin.
+                C:\Users\aksma\git\react-assignment-1>git status
+                On branch step-1 Your branch is up-to-date with 'origin/step-1'.
+                Untracked files: (use "git add \<file\>..." to include in what will be committed)
+            </p>
+            <p>
+                To https://github.com/akshaypalshikar/react-assignment-1.git
+                * [new branch] step-1 -> step-1
+                Branch step-1 set up to track remote branch step-1 from origin.
+                C:\Users\aksma\git\react-assignment-1>git status
+                On branch step-1 Your branch is up-to-date with 'origin/step-1'.
+                Untracked files: (use "git add \<file\>..." to include in what will be committed)
+            </p>
+        </div>
+    );
+}
